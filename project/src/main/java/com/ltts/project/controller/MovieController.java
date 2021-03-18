65065b5b64a9bf5e10890263e5e112486b82d656
VinayakAlagwadi <78849168+VinayakAlagwadi@users.noreply.github.com>
2021-03-18 14:23:12 +0530

    Add files via upload

---
+++ project/src/main/java/com/ltts/project/controller/MovieController.java
@@ -0,0 +1,16 @@
+package com.ltts.project.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ltts.project.Dao.MovieDao;
+
+@RestController
+public class MovieController {
+	
+	MovieDao md;
+	
+	
+	
+	
+
+}
